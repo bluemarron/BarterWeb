9be02bb96b238e0881c10724de1e1cc36f0176c9
bluemarron <jack.bluemarron@gmail.com>
2013-07-07 00:24:37 +0900

    .

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>다사다 - 물물교환</title>
+    <title>다사다 물물교환</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
