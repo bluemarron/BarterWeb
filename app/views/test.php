865d8ccd471db0fa00e9dabb77a08b71209c2551
bluemarron <jack.bluemarron@gmail.com>
2014-03-10 00:05:58 +0900

    fixed html tags

--- app/views/test.php
+++ app/views/test.php
@@ -1,6 +1,8 @@
 <script>
 function preview(input) {
-	document.getElementById("test").src = input.value;
+	//document.getElementById("test").src = input.value;
+	document.getElementById("test").src = 	"http://img.naver.net/static/www/u/2013/0731/nmms_224940510.gif";
+
 }
 </script>
 <img id="test" src="">
