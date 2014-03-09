42487f7215ae97f30acde0961b6991408edd686f
bluemarron <jack.bluemarron@gmail.com>
2014-03-10 00:37:30 +0900

    fixed html tags

--- app/views/test.php
+++ app/views/test.php
@@ -1,8 +1,28 @@
-<script>
-function preview(input) {
-	document.getElementById("test").src = input.value;
-	
-}
+<style type="text/css">
+    #prevImage {
+        border: 8px solid #ccc;
+        width: 300px;
+        height: 200px;
+    }
+</style>
+<script type="text/javascript">
+    function setImage(file) {
+        if(document.all)
+            document.getElementById('prevImage').src = file.value;
+        else
+            document.getElementById('prevImage').src = file.files.item(0).getAsDataURL();
+        if(document.getElementById('prevImage').src.length > 0) 
+            document.getElementById('prevImage').style.display = 'block';
+    }
 </script>
-<img id="test" src="">
-<input type="file" id="file_1" onchange="preview(this)">
+<pre>
+     IE8 needs a security settings change: internet settings, security, custom level :
+
+     [] Include local directory path when uploading files to a server
+ ( ) Disable
+ (o) Enable 
+</pre>
+<form>
+    <input type="file" name="myImage" onchange="setImage(this);" />
+</form>
+<img id="prevImage" style="display:none;" />
